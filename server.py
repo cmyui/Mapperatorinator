b21519c19d24f15fffd0f79cea18558067352d30
cmyui <cmyuiosu@gmail.com>
2025-06-14 09:06:29 -0400

    server

--- server.py
+++ server.py
@@ -1,36 +1,72 @@
-from pydantic import BaseModel
-import inference
-
-
-from fastapi import FastAPI
+import subprocess
+import sys
+import tempfile
+from fastapi import FastAPI, File, Response, UploadFile
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 
-class InferenceRequest(BaseModel):
-    audio_path: str
-    output_path: str
-    gamemode: int
-    difficulty: float
+def hydra_quote(value):
+    """Quotes a value for Hydra (single quotes, escapes internal)."""
+    value_str = str(value)
+    # Escape internal single quotes: ' -> '\''
+    escaped_value = value_str.replace("'", r"\'")
+    return f"'{escaped_value}'"
 
 
 @app.post("/ai-maps")
-async def generate_map(request: InferenceRequest):
-    """
-    Endpoint to generate a map using the provided request data.
-    """
+async def generate_map(
+    difficulty: float,
+    audio_file: UploadFile = File(...),
+):
     try:
-        response = await inference.main(
-            inference.InferenceConfig(
-                audio_path=request.audio_path,
-                output_path=request.output_path,
-                gamemode=request.gamemode,
-                difficulty=request.difficulty,
-                export_osz=True,
-            ),
-        )
-        with open(response[2], "rb") as f:
-            osz_content = f.read()
-        return osz_content
+        assert audio_file.filename is not None
+        file_extension = audio_file.filename.rsplit(".", maxsplit=1)[-1]
+        with (
+            tempfile.NamedTemporaryFile(suffix=f".{file_extension}") as input_tempfile,
+            tempfile.NamedTemporaryFile(suffix=".osz", delete=False) as output_tempfile,
+        ):
+            # Save the uploaded audio file to a temporary file
+            input_tempfile.write(await audio_file.read())
+
+            cmd = [
+                sys.executable,
+                "inference.py",
+                "-cn",
+                "inference_v30",
+                "gamemode=0",
+                f"audio_path={hydra_quote(input_tempfile.name)}",
+                f"output_path={hydra_quote(output_tempfile.name)}",
+                f"difficulty={difficulty}",
+                "export_osz=true",
+                "super_timing=false",
+                "hitsounded=false",
+                "add_to_beatmap=false",
+            ]
+            current_process = subprocess.Popen(
+                cmd,
+                shell=False,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                bufsize=1,
+                universal_newlines=True,
+                encoding="utf-8",
+                errors="replace",
+            )
+            print(
+                f"Generating AI map ({input_tempfile.name} -> {output_tempfile.name})"
+            )
+            stdout, _ = current_process.communicate()
+            print(stdout)
+
+            return FileResponse(output_tempfile.name)
+
     except Exception as e:
-        return {"error": str(e)}
+        return Response({"error": str(e)}, status_code=500)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("server:app", reload=True)
